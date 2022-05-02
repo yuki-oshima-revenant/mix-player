3074de4f66fbcabfb93d64c5eb0438725611a91a
yuki-oshima-revenant <revenant.jumboride@gmail.com>
2022-05-02 18:38:40 +0900

    fix width

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import type { NextPage } from 'next';
 import Head from "next/head";
 import { useRouter } from 'next/router';
 import { data } from '../lib/data'
-import { ImPlay2, ImPause, ImVolumeHigh, ImTwitter, ImGithub } from 'react-icons/im';
+import { ImTwitter, ImGithub } from 'react-icons/im';
 import { useIsMobile } from '../lib/utils/useIsMobile';
+import { useRef } from 'react';
 
 const Home: NextPage = () => {
     const router = useRouter();
     const isMobile = useIsMobile();
+    const ref = useRef<HTMLDivElement>(null);
     return (
-        <div>
+        <div ref={ref}>
             <Head>
                 <title>{`mixplayer.unronritaro.net`}</title>
                 <meta property="og:title" content={`mixplayer.unronritaro.net`} />
@@ -17,7 +19,7 @@ const Home: NextPage = () => {
                 <meta name="twitter:image" content={`https://mixplayer.unronritaro.net/ogp/top.png`} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
-            <div className="bg-black text-white">
+            <div className="bg-black text-white w-full">
                 <header className="fixed z-[5] flex w-full px-2 lg:px-4 h-12">
                     <div className='flex-grow' />
                     <div className='text-xl flex'>
@@ -36,7 +38,7 @@ const Home: NextPage = () => {
                     </div>
                 </header>
                 {/* <h1>mix list</h1> */}
-                <div style={{ height: `calc(100vh - 32px)`, minHeight: `calc(${data.length * (isMobile ? 160 : 320)}px + 32px + 32px)` }} className="pt-12 w-screen">
+                <div style={{ height: `calc(100vh - 32px)`, minHeight: `calc(${data.length * (isMobile ? 160 : 320)}px + 32px + 32px)` }} className="pt-12 w-full">
                     {
                         data.map((mix, mixIndex) => (
                             <div key={mix.title} className="flex h-[160px] lg:h-[320px] cursor-pointer group" onClick={() => { router.push(`/mix/${mixIndex + 1}`) }}>
@@ -54,7 +56,7 @@ const Home: NextPage = () => {
                                     {/* <div className="text-lg mt-2">{mix.time}</div> */}
                                 </div>
                                 <div className='absolute overflow-hidden'>
-                                    <div className='flex w-screen h-[160px] lg:h-[320px]'>
+                                    <div className='flex h-[160px] lg:h-[320px]' style={{ width: ref.current?.getBoundingClientRect().width }}>
                                         {mix.tracks.map((track, trackIndex) => {
                                             return (
                                                 <div key={track.title} className="h-[160px] lg:h-[320px] w-[160px] lg:w-[320px] absolute" style={{ left: trackIndex * (isMobile ? 80 : 160) }}>
