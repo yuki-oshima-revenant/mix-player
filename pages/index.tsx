182a1017f6bff68a437c043e8ca596cb0a4193db
yuki-oshima-revenant <revenant.jumboride@gmail.com>
2022-05-08 14:09:51 +0900

    fix order

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,15 @@ import { useRouter } from 'next/router';
 import { data } from '../lib/data'
 import { ImTwitter, ImGithub } from 'react-icons/im';
 import { useWindowSize } from '../lib/utils/useIsMobile';
-import { useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 
 const Home: NextPage = () => {
     const router = useRouter();
     const { width, isMobile } = useWindowSize();
     const ref = useRef<HTMLDivElement>(null);
+    const listData = useMemo(() => {
+        return [...data].map((mix, index) => ({ ...mix, index })).reverse();
+    }, []);
     return (
         <div ref={ref}>
             <Head>
@@ -37,39 +40,28 @@ const Home: NextPage = () => {
                         />
                     </div>
                 </header>
-                {/* <h1>mix list</h1> */}
                 <div style={{ height: `calc(100vh - 32px)`, minHeight: `calc(${data.length * (isMobile ? 160 : 320)}px + 32px + 32px)` }} className="pt-12 w-full">
-                    {
-                        data.map((mix, mixIndex) => (
-                            <div key={mix.title} className="flex h-[160px] lg:h-[320px] cursor-pointer group" onClick={() => { router.push(`/mix/${mixIndex + 1}`) }}>
-                                <div className='z-[3] py-1 px-2 lg:px-4'>
-                                    <h2 className='text-3xl lg:text-6xl font-bold tracking-tighter' >
-                                        {mix.title}
-                                    </h2>
-                                    {/* <div className="flex gap-1 lg:gap-2 mt-2">
-                                    {mix.genres.map(({ name, color },) => (
-                                        <div className={`px-2 font-medium`} key={name} style={{ backgroundColor: color }}>
-                                            {name}
-                                        </div>
-                                    ))}
-                                </div> */}
-                                    {/* <div className="text-lg mt-2">{mix.time}</div> */}
-                                </div>
-                                <div className='absolute overflow-hidden'>
-                                    <div className='flex h-[160px] lg:h-[320px]' style={{ width }}>
-                                        {mix.tracks.map((track, trackIndex) => {
-                                            return (
-                                                <div key={track.title} className="h-[160px] lg:h-[320px] w-[160px] lg:w-[320px] absolute" style={{ left: trackIndex * (isMobile ? 80 : 160) }}>
-                                                    <img src={track.imageLink} className="h-[160px] lg:h-[320px] w-[160px] lg:w-[320px]" />
-                                                </div>
-                                            )
-                                        })}
-                                        <div className="h-[160px] lg:h-[320px] absolute w-full z-[2] backdrop-blur-xl group-hover:backdrop-blur-0 duration-150 bg-black/30" />
-                                    </div>
+                    {listData.map((mix) => (
+                        <div key={mix.title} className="flex h-[160px] lg:h-[320px] cursor-pointer group" onClick={() => { router.push(`/mix/${mix.index + 1}`) }}>
+                            <div className='z-[3] py-1 px-2 lg:px-4'>
+                                <h2 className='text-3xl lg:text-6xl font-bold tracking-tighter' >
+                                    {mix.title}
+                                </h2>
+                            </div>
+                            <div className='absolute overflow-hidden' style={{ width }}>
+                                <div className='flex h-[160px] lg:h-[320px]'>
+                                    {mix.tracks.map((track, trackIndex) => {
+                                        return (
+                                            <div key={track.title} className="h-[160px] lg:h-[320px] w-[160px] lg:w-[320px] absolute" style={{ left: trackIndex * (isMobile ? 80 : 160) }}>
+                                                <img src={track.imageLink} className="h-[160px] lg:h-[320px] w-[160px] lg:w-[320px]" />
+                                            </div>
+                                        )
+                                    })}
+                                    <div className="h-[160px] lg:h-[320px] absolute w-full z-[2] backdrop-blur-xl group-hover:backdrop-blur-0 duration-150 bg-black/30" />
                                 </div>
                             </div>
-                        ))
-                    }
+                        </div>
+                    ))}
                 </div>
                 <footer className="h-8 text-center">©︎2022 Yuki Oshima</footer>
             </div>
